9b2e38113e0e91336bf51724b4dc22ac9372dcbf
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2012-10-17 01:58:56 +0000

    Plan to use hibernate with shiro.

---
+++ trunk/mavenized-web/src/main/java/com/osm/model/Role.java
@@ -0,0 +1,36 @@
+package com.osm.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+
+    @Id
+    @GeneratedValue(generator = "hibernate-uuid")
+    @GenericGenerator(name = "hibernate-uuid", strategy = "uuid")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ trunk/mavenized-web/src/main/java/com/osm/model/User.java
@@ -0,0 +1,36 @@
+package com.osm.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private String id;
+    private String username;
+
+    @Id
+    @GeneratedValue(generator = "hibernate-uuid")
+    @GenericGenerator(name = "hibernate-uuid", strategy = "uuid")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(name = "username", unique = true, nullable = false, updatable = false)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
