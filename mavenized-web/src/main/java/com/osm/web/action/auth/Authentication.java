9e7b03b95e80b5e97d32463bc964a36827398ebd
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2012-10-17 01:59:26 +0000

    provided log message for Authentication action

--- mavenized-web/src/main/java/com/osm/web/action/auth/Authentication.java
+++ mavenized-web/src/main/java/com/osm/web/action/auth/Authentication.java
@@ -4,24 +4,30 @@ import com.google.common.base.Strings;
 import com.opensymphony.xwork2.ActionSupport;
 import com.osm.auth.AuthenticationException;
 import com.osm.auth.Authenticator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("serial")
 public class Authentication extends ActionSupport {
-
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(Authentication.class);
     private Authenticator authenticator;
-
+    
     public String login() throws Exception {
         if (Strings.isNullOrEmpty(getUsername())) {
             return INPUT;
         }
-
+        
         if (Strings.isNullOrEmpty(getPassword())) {
             return INPUT;
         }
-
+        
+        LOGGER.debug("authenticating {}", username);
         try {
             authenticator.login(username, password);
+            LOGGER.debug("user {} is authenticated", username);
         } catch (AuthenticationException e) {
+            LOGGER.debug("authentication error: {}", e.getMessage());
             return INPUT;
         }
         return SUCCESS;
@@ -31,26 +37,25 @@ public class Authentication extends ActionSupport {
         authenticator.logout();
         return SUCCESS;
     }
-
     private String username;
-
+    
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
     private String password;
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public void setAuthenticator(final Authenticator authenticator) {
         this.authenticator = authenticator;
     }
