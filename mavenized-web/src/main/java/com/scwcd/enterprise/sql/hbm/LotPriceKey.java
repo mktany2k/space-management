4a49dcd744df77f444ef1646fbc4dc7514287d78
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2012-10-17 01:59:50 +0000

    change the toString method to use google guava objects.

--- mavenized-web/src/main/java/com/scwcd/enterprise/sql/hbm/LotPriceKey.java
+++ mavenized-web/src/main/java/com/scwcd/enterprise/sql/hbm/LotPriceKey.java
@@ -1,44 +1,41 @@
 package com.scwcd.enterprise.sql.hbm;
 
+import com.google.common.base.Objects;
 
 public class LotPriceKey extends LotKey {
 
-	private static final long serialVersionUID = 1001L;
+    private static final long serialVersionUID = 1001L;
+    private int priceId;
 
-	private int priceId;
+    public LotPriceKey() {
+    }
 
-	public LotPriceKey() {
-	}
+    public LotPriceKey(final int priceId) {
+        this.priceId = priceId;
+    }
 
-	public LotPriceKey(final int priceId) {
-		this.priceId = priceId;
-	}
+    public void setPriceId(int priceId) {
+        this.priceId = priceId;
+    }
 
-	public void setPriceId(int priceId) {
-		this.priceId = priceId;
-	}
+    public int getPriceId() {
+        return priceId;
+    }
 
-	public int getPriceId() {
-		return priceId;
-	}
-
-	@Override
+    @Override
     public int hashCode() {
-		final int hashCode = super.hashCode();
-		return hashCode + priceId;
-	}
+        final int hashCode = super.hashCode();
+        return hashCode + priceId;
+    }
 
-	@Override
-	public String toString() {
-		final int projectId = getProjectId();
-		final int planId = getPlanId();
-		final int lotId = getLotId();
-		StringBuilder sbString = new StringBuilder();
-		sbString.append("LotPriceKey[");
-		sbString.append(projectId).append(", ");
-		sbString.append(planId).append(", ");
-		sbString.append(lotId).append(", ");
-		sbString.append(priceId).append("]");
-		return sbString.toString();
-	}
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .addValue(getProjectId())
+                .addValue(getPlanId())
+                .addValue(getLotId())
+                .addValue(getPriceId())
+                .omitNullValues()
+                .toString();
+    }
 }
