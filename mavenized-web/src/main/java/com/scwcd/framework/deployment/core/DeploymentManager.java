f7d57c239a800cb4f83bc8246385cc293122e536
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2012-11-21 06:05:19 +0000

    Decide to remove this class after 5 Dec 2012. This is because I don't found a use case for this. I like remove unused code

--- mavenized-web/src/main/java/com/scwcd/framework/deployment/core/DeploymentManager.java
+++ mavenized-web/src/main/java/com/scwcd/framework/deployment/core/DeploymentManager.java
@@ -1,6 +1,6 @@
 package com.scwcd.framework.deployment.core;
 
-
+import com.google.common.collect.Maps;
 import com.scwcd.enterprise.sql.dao.DAOProject;
 import com.scwcd.enterprise.sql.dao.ProjectParameter;
 import com.scwcd.enterprise.sql.hbm.Plan;
@@ -10,88 +10,88 @@ import com.scwcd.framework.sql.core.DAOFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import org.apache.camel.Processor;
 
+/**
+ * @deprecated This class will be removed.
+ */
+@Deprecated
+public final class DeploymentManager {
 
-public class DeploymentManager {
+    private static final DeploymentManager INSTANCE = new DeploymentManager();
+    private CamelManager camelManager;
+    private final Map<String, Integer> hashtable;
 
-	private static final DeploymentManager INSTANCE = new DeploymentManager();
+    private DeploymentManager() {
+        this.hashtable = Maps.newConcurrentMap();
+    }
 
-	private CamelManager camelManager;
-	
-	private final Map<String, Integer> hashtable = new ConcurrentHashMap<>();
+    @Deprecated
+    public static DeploymentManager getInstance() {
+        return INSTANCE;
+    }
 
-	private DeploymentManager() {
-		// prevent instantiation
-	}
+    @Deprecated
+    public void init() {
+        // retrieve project directory path
 
-	public static DeploymentManager getInstance() {
-		return INSTANCE;
-	}
+        // retrieve all projects
+        final DAOFactory factory = DAOFactory.getInstance();
+        final DAOProject dao = (DAOProject) factory.getInstance(DAOProject.class);
+        // for every projects, create a new processor and a new parser
+        List<Project> projects = dao.doList();
+        final List<Processor> processors = new ArrayList<>(projects.size());
+        for (Project project : dao.doList()) {
+            for (Plan plan : project.getPlans()) {
+                hashtable.put(plan.getFilename(), plan.getPlanId());
+            }
 
-	public void init() {
-		// retrieve project directory path
-		final WebContext ctx = WebContextManager.getWebContext();
-		final String configPath = ctx.getProjectPath();
+            final XsltTemplate parserType = XsltTemplate.getTemplate(project.getParser());
+            final int projectId = project.getProjectId();
 
-		// retrieve all projects
-		final DAOFactory factory = DAOFactory.getInstance();
-		final DAOProject dao = (DAOProject) factory.getInstance(DAOProject.class);
+            final DefaultDeploymentProcessor processor = new DefaultDeploymentProcessor();
+            processor.setProjectId(projectId);
+            processor.setProjectPath(WebContextManager.getWebContext().getProjectPath());
+            processor.setXsltTemplate(parserType);
+            processor.setHashtable(hashtable);
 
-		// for every projects, create a new processor and a new parser
-		final List<Processor> processors = new ArrayList<>();
-		for (Project project : dao.doList()) {
-			for (Plan plan : project.getPlans()) {
-				hashtable.put(plan.getFilename(), plan.getPlanId());
-			}
+            processors.add(processor);
+        }
 
-			final XsltTemplate parserType = XsltTemplate.getTemplate(project.getParser());
-			final int projectId = project.getProjectId();
+        try {
+            camelManager = new CamelManager(processors);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			final DefaultDeploymentProcessor processor = new DefaultDeploymentProcessor();
-			processor.setProjectId(projectId);
-			processor.setProjectPath(configPath);
-			processor.setXsltTemplate(parserType);
-			processor.setHashtable(hashtable);
+    public void start() {
+        if (!camelManager.isRunning()) {
+            camelManager.start();
+        }
+    }
 
-			processors.add(processor);
-		}
+    public void listen(final int projectId, final String configPath) {
+        if (camelManager.isRunning()) {
+            final DAOFactory factory = DAOFactory.getInstance();
+            final DAOProject dao = (DAOProject) factory.getInstance(DAOProject.class);
+            final Project project = dao.doSelect(new ProjectParameter(projectId));
 
-		try {
-			camelManager = new CamelManager(processors);
-		} catch (final Exception e) {
-			e.printStackTrace();
-		}
-	}
+            final XsltTemplate xsltTemplate = XsltTemplate.getTemplate(project.getParser());
 
-	public void start() {
-		if (!camelManager.isRunning()) {
-			camelManager.start();
-		}
-	}
+            final DefaultDeploymentProcessor processor = new DefaultDeploymentProcessor();
+            processor.setProjectId(projectId);
+            processor.setProjectPath(configPath);
+            processor.setXsltTemplate(xsltTemplate);
+            processor.setHashtable(hashtable);
 
-	public void listen(final int projectId, final String configPath) {
-		if (camelManager.isRunning()) {
-			final DAOFactory factory = DAOFactory.getInstance();
-			final DAOProject dao = (DAOProject) factory.getInstance(DAOProject.class);
-			final Project project = dao.doSelect(new ProjectParameter(projectId));
+            camelManager.load(processor);
+        }
+    }
 
-			final XsltTemplate xsltTemplate = XsltTemplate.getTemplate(project.getParser());
-
-			final DefaultDeploymentProcessor processor = new DefaultDeploymentProcessor();
-			processor.setProjectId(projectId);
-			processor.setProjectPath(configPath);
-			processor.setXsltTemplate(xsltTemplate);
-			processor.setHashtable(hashtable);
-
-			camelManager.load(processor);
-		}
-	}
-
-	public void stop() {
-		if (camelManager.isRunning()) {
-			camelManager.stop();
-		}
-	}
+    public void stop() {
+        if (camelManager.isRunning()) {
+            camelManager.stop();
+        }
+    }
 }
