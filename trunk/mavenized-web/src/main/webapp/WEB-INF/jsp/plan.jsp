b634caf37518c51b3f83e21bdb66c1b20a979b6b
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2012-12-10 07:44:15 +0000

    upgrade twitter bootstrap from 2.2.1 to 2.2.2 clean up exsitng code add more dummy content in adminstrator page create more unit test remove unneeded test jar (this time remove struts-test-plugin) will add it back if needed

--- trunk/mavenized-web/src/main/webapp/WEB-INF/jsp/plan.jsp
+++ trunk/mavenized-web/src/main/webapp/WEB-INF/jsp/plan.jsp
@@ -1,6 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix='fn' uri="http://java.sun.com/jsp/jstl/functions"%>
-<%@ taglib prefix="stl" uri="SvgTagLibrary" %>
 <html>
 	<head>
 		<title><fmt:message key="project.name"/></title>
---
+++ trunk/mavenized-web/src/test/java/com/osm/util/ClassWithPrivateConstructorUnitTest.java
@@ -0,0 +1,44 @@
+package com.osm.util;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ClassWithPrivateConstructorUnitTest {
+    @Test(dataProvider = "private classes")
+    public void cannot_instantiate_via_reflection(Class<?> classWithPrivateConstructor) {
+        try {
+            final Constructor<?> constructor = classWithPrivateConstructor.getDeclaredConstructor();
+            assertThat(constructor).isNotNull();
+            AccessController.doPrivileged(new PrivilegedAction<Void>() {
+                @Override
+                public Void run() {
+                    constructor.setAccessible(true);
+                    return null;
+                }
+            });
+            constructor.newInstance();
+            fail(String.format("The class %s shouldn't be instantiated", classWithPrivateConstructor.getName()));
+        } catch (InvocationTargetException ex) {
+            assertThat(ex.getCause()).isNotNull()
+                    .isInstanceOf(InstantiationException.class)
+                    .hasMessage(String.format("The class %s shouldn't be instantiated", classWithPrivateConstructor.getName()));
+        } catch (ReflectiveOperationException | SecurityException ex) {
+            fail(ex.getMessage(), ex);
+        }
+    }
+
+    @DataProvider(name = "private classes", parallel = true)
+    public Object[][] private_classes() {
+        return new Object[][]{
+                    {Exceptions.class},
+                    {Constants.HibernateGenerator.class}
+                };
+    }
+
+}
