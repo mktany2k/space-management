28b4f113343fab123ff42a37071530851f43872e
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2012-12-11 09:51:29 +0000

    Tidy up

--- trunk/mavenized-web/src/main/java/com/osm/model/User.java
+++ trunk/mavenized-web/src/main/java/com/osm/model/User.java
@@ -19,14 +19,6 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Index;
 
-/**
- * Simple class that represents any User domain entity in any application.
- *
- * <p>Because this class performs its own Realm and Permission checks, and these can happen frequently enough in a production
- * application, it is highly recommended that the internal User {@link #getRoles} collection be cached in a 2nd-level cache when
- * using JPA and/or Hibernate. The hibernate xml configuration for this sample application does in fact do this for your reference
- * (see User.hbm.xml - the 'roles' declaration).</p>
- */
 @Entity
 @Table(name = "users", uniqueConstraints =
 @UniqueConstraint(name = "uniq_username", columnNames = "username"))
