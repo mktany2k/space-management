71ca600f130d0ec51b4cdaf851bdcc4b3c442185
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2013-01-04 07:48:06 +0000

    minor enhancement

--- trunk/mavenized-web/src/main/java/com/osm/model/LotKey.java
+++ trunk/mavenized-web/src/main/java/com/osm/model/LotKey.java
@@ -1,64 +1,74 @@
 package com.osm.model;
 
-
 import java.io.Serializable;
-
+import java.util.Objects;
 
 public class LotKey implements Serializable {
 
-	private static final long serialVersionUID = 1001L;
+    private static final long serialVersionUID = 1001L;
+    private int projectId;
+    private int planId;
+    private int lotId;
 
-	private int projectId;
+    public LotKey() {
+    }
 
-	private int planId;
+    public LotKey(final int projectId, final int planId, final int lotId) {
+        this.projectId = projectId;
+        this.planId = planId;
+        this.lotId = lotId;
+    }
 
-	private int lotId;
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
 
-	public LotKey() {
-	}
+    public int getProjectId() {
+        return projectId;
+    }
 
-	public LotKey(final int projectId, final int planId, final int lotId) {
-		this.projectId = projectId;
-		this.planId = planId;
-		this.lotId = lotId;
-	}
+    public void setPlanId(int planId) {
+        this.planId = planId;
+    }
 
-	public void setProjectId(int projectId) {
-		this.projectId = projectId;
-	}
+    public int getPlanId() {
+        return planId;
+    }
 
-	public int getProjectId() {
-		return projectId;
-	}
+    public void setLotId(int lotId) {
+        this.lotId = lotId;
+    }
 
-	public void setPlanId(int planId) {
-		this.planId = planId;
-	}
+    public int getLotId() {
+        return lotId;
+    }
 
-	public int getPlanId() {
-		return planId;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(projectId, planId, lotId);
+    }
 
-	public void setLotId(int lotId) {
-		this.lotId = lotId;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
 
-	public int getLotId() {
-		return lotId;
-	}
+        LotKey other = (LotKey) obj;
+        return Objects.equals(projectId, other.projectId)
+                && Objects.equals(planId, other.planId)
+                && Objects.equals(lotId, other.lotId);
+    }
 
-	@Override
-	public int hashCode() {
-		return (projectId * 10000000) + (planId * 10000) + (lotId);
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sbString = new StringBuilder();
-		sbString.append("LotKey[");
-		sbString.append(projectId).append(", ");
-		sbString.append(planId).append(", ");
-		sbString.append(lotId).append("]");
-		return sbString.toString();
-	}
+    @Override
+    public String toString() {
+        return com.google.common.base.Objects.toStringHelper(this)
+                .addValue(projectId)
+                .addValue(planId)
+                .addValue(lotId)
+                .toString();
+    }
 }
---
+++ trunk/mavenized-web/src/main/java/com/osm/web/action/auth/UserAction.java
@@ -0,0 +1,34 @@
+package com.osm.web.action.auth;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.osm.model.User;
+import com.osm.repository.UserRepository;
+import java.util.Collection;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("user")
+@Scope("prototype")
+public class UserAction extends ActionSupport {
+
+    private static final long serialVersionUID = 1L;
+    private UserRepository userRepository;
+
+    @Action(value = "/admin/listUser", results = {
+        @Result(location = "/WEB-INF/jsp/admin/administration.jsp")})
+    public String list() throws Exception {
+        return SUCCESS;
+    }
+
+    public Collection<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
