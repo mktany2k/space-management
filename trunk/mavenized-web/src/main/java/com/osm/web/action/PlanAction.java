62ff3096e007a2c27418ed6284ab80644c270bb5
mktany2k@gmail.com <mktany2k@gmail.com@80936f46-05bf-99f2-3394-c9a0ba210972>
2013-01-27 17:41:41 +0000

    change tab to space.

--- trunk/mavenized-web/src/main/java/com/osm/web/action/PlanAction.java
+++ trunk/mavenized-web/src/main/java/com/osm/web/action/PlanAction.java
@@ -1,31 +1,28 @@
 package com.osm.web.action;
 
-
 import com.opensymphony.xwork2.ActionSupport;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts2.interceptor.ServletRequestAware;
 
-
 public class PlanAction extends ActionSupport implements ServletRequestAware {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private HttpServletRequest request;
 
-	private HttpServletRequest request;
-
-	/*
-	 * Get the number of .svg files in /WEB-INF/dat.
-	 *
-	 * (non-Javadoc)
-	 * @see com.opensymphony.xwork2.ActionSupport#execute()
-	 */
-	@Override
+    /*
+     * Get the number of .svg files in /WEB-INF/dat.
+     *
+     * (non-Javadoc)
+     * @see com.opensymphony.xwork2.ActionSupport#execute()
+     */
+    @Override
     public String execute() {
-		// read floor-plan.svg files
-		return SUCCESS;
-	}
+        // read floor-plan.svg files
+        return SUCCESS;
+    }
 
-	@Override
-	public void setServletRequest(final HttpServletRequest request) {
-		this.request = request;
-	}
+    @Override
+    public void setServletRequest(final HttpServletRequest request) {
+        this.request = request;
+    }
 }
